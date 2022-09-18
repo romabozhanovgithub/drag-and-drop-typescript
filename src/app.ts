5c07c422974ef78d6c90724fa471f670d9504fb6
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2022-09-18 19:30:02 +0300

    add project state

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,46 @@
+// Proejct State Management
+class ProjectState { // ProjectState class, which manages the state of the projects
+    private listeners: any[] = [];
+    private projects: any[] = [];
+    private static instance: ProjectState;
+
+    private constructor() {} // private constructor, so that no one can create new instance of this class
+
+    static getInstance() { // static method, so that we can call this method without creating new instance of this class
+        if (this.instance) { // if instance already exists, return it
+            return this.instance;
+        }
+        this.instance = new ProjectState(); // if instance does not exist, create a new instance and return it
+        return this.instance;
+    }
+
+    addListener(listenerFn: Function) { // listenerFn is a function that takes an array of projects as an argument and returns void
+        this.listeners.push(listenerFn); // push the listener function to the listeners array
+    }
+
+    addProject(title: string, description: string, numOfPeople: number) { // add a new project to the projects array, and call all the listener functions
+        const newProject = { // create a new project
+            id: Math.random().toString(),
+            title,
+            description,
+            people: numOfPeople,
+        };
+        this.projects.push(newProject); // push the new project to the projects array
+        for (const listenerFn of this.listeners) { // call all the listener functions with the projects array as an argument, so that they can update their UI
+            listenerFn(this.projects.slice()); // slice() is used to create a copy of the projects array, so that the original array is not modified
+        }
+    }
+}
+
+const projectState = ProjectState.getInstance(); // create a new instance of the ProjectState class
+
 // autobind decorator
-const autobind = (_: any, _2: string, descriptor: PropertyDescriptor) => {
-    const originalMethod = descriptor.value;
-    const adjDescriptor: PropertyDescriptor = {
-        configurable: true,
+const autobind = (_: any, _2: string, descriptor: PropertyDescriptor) => { // _ is unused, _2 is the name of the method, descriptor is the property descriptor
+    const originalMethod = descriptor.value; // store the original method
+    const adjDescriptor: PropertyDescriptor = { // adjDescriptor is the adjusted descriptor, which is the descriptor we return
+        configurable: true, // we can reconfigure this property
         get() {
-            const boundFn = originalMethod.bind(this);
+            const boundFn = originalMethod.bind(this); // bind the original method to the instance of the class
             return boundFn;
         }
     };
@@ -14,7 +50,7 @@ const autobind = (_: any, _2: string, descriptor: PropertyDescriptor) => {
 // Validation interface
 interface Validatable {
     value: string | number;
-    required?: boolean;
+    required?: boolean; // ? means optional
     minLength?: number;
     maxLength?: number;
     min?: number;
@@ -22,25 +58,70 @@ interface Validatable {
 }
 
 // Validation
-const validate = (validatableInput: Validatable) => {
+function validate(validatableInput: Validatable) {
     let isValid = true;
     if (validatableInput.required) {
-        isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+        isValid = isValid && validatableInput.value.toString().trim().length !== 0; // trim() removes whitespace
     }
     if (validatableInput.minLength != null && typeof validatableInput.value === 'string') {
-        isValid = isValid && validatableInput.value.length > validatableInput.minLength;
+        isValid = isValid && validatableInput.value.length >= validatableInput.minLength;
     }
     if (validatableInput.maxLength != null && typeof validatableInput.value === 'string') {
-        isValid = isValid && validatableInput.value.length < validatableInput.maxLength;
+        isValid = isValid && validatableInput.value.length <= validatableInput.maxLength;
     }
     if (validatableInput.min != null && typeof validatableInput.value === 'number') {
-        isValid = isValid && validatableInput.value > validatableInput.min;
+        isValid = isValid && validatableInput.value >= validatableInput.min;
     }
     if (validatableInput.max != null && typeof validatableInput.value === 'number') {
-        isValid = isValid && validatableInput.value < validatableInput.max;
+        isValid = isValid && validatableInput.value <= validatableInput.max;
     }
     return isValid;
-};
+}
+
+// ProjectList class
+class ProjectList {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLElement;
+    assignedProjects: any[];
+
+    constructor(private type: 'active' | 'finished') { // private type: 'active' | 'finished' means that type can only be active or finished
+        this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+        this.assignedProjects = [];
+
+        const importedNode = document.importNode(this.templateElement.content, true); // true means deep clone, false means shallow clone
+        this.element = importedNode.firstElementChild as HTMLElement;
+        this.element.id = `${this.type}-projects`; // this.element.id = `${this.type}-projects` means that the id of the element will be active-projects or finished-projects
+
+        projectState.addListener((projects: any[]) => { // add a listener to the projectState
+            this.assignedProjects = projects; // store the projects array in the assignedProjects array
+            this.renderProjects(); // render the projects
+        });
+
+        this.attach();
+        this.renderContent();
+    }
+
+    private renderProjects() {
+        const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
+        for (const prjItem of this.assignedProjects) {
+            const listItem = document.createElement('li');
+            listItem.textContent = prjItem.title;
+            listEl.appendChild(listItem);
+        }
+    }
+
+    private renderContent() {
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector('ul')!.id = listId;
+        this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS';
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
+    }
+}
 
 // ProjectInput Class
 class ProjectInput {
@@ -86,7 +167,7 @@ class ProjectInput {
             minLength: 5
         };
         const peopleValidatable: Validatable = { // object literal, type is inferred
-            value: +enteredPeople,
+            value: +enteredPeople, // + is a type casting operator, it converts the string to a number
             required: true,
             min: 1,
             max: 5
@@ -110,7 +191,7 @@ class ProjectInput {
         const userInput = this.gatherUserInput();
         if (Array.isArray(userInput)) {
             const [title, description, people] = userInput;
-            console.log(title, description, people);
+            projectState.addProject(title, description, people);
             this.clearInputs();
         }
     }
@@ -128,8 +209,10 @@ class ProjectInput {
     }
 
     private attach() { // this is a good place to attach the element to the DOM
-        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+        this.hostElement.insertAdjacentElement('afterbegin', this.element); // insertAdjacentElement is a method of the Element interface, it takes two arguments, the first is a string, the second is an element
     }
 }
 
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList('active');
+const finishedPrjList = new ProjectList('finished');
